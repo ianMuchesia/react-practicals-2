f80499b60276ae5e13d2dc214aaae44b6822075c
ianMuchesia <ianmuchesia@yahoo.com>
2023-02-03 11:52:38 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react'
+
+const App = () => {
+  const images=[
+    {url:  "http://localhost:5174/1.png",title: "1" },
+    {url:  "http://localhost:5174/2.png",title: "2" },
+    {url:  "http://localhost:5174/3.png",title: "3" },
+    {url:  "http://localhost:5174/4.png",title: "4" },
+    {url:  "http://localhost:5174/5.png",title: "5" },
+  ]
+    
+  
+  return (
+  <img src="http://localhost:5174/1.png"/>
+  )
+}
+
+export default App
